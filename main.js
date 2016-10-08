f7c1be33e788355d7f115f8f01b5911d1baec9e3
Tom Anderson <portlester.tom@gmail.com>
2016-10-08 21:53:47 +1100

    Finish matrix

--- main.js
+++ main.js
@@ -1,16 +1,4 @@
-var characters = [0,1,2,3,4,5,6,7,8,9]
-
-function init() {
-	// Gets column number from window width
-	var columns = Math.floor(window.innerWidth / 10);
-
-	// Add enough columns to take up full width of window
-	for (i = 0; i < columns; i++) {
-		$("#container").append('<div id="col' + i + '" class="column"></div>');
-	}
-	// For test purposes. Will need to be automated so that it will automatically pick column ect.
-	addCell(pickColumn());
-}
+var characters = [0,1,2,3,4,5,6,7,8,9, 'a', 'b', 'c', 'd']
 
 function randomCharacter() {
 	// Returns a random character from the array that holds all of the characters used in the matrix
@@ -19,13 +7,31 @@ function randomCharacter() {
 
 function addCell(columnNum) {
 	// Adds a cell to the provided column with a random character inside
-	$("#col" + columnNum).append('<p class="cell">' + randomCharacter() + '</p>')
+	$("#col" + columnNum).prepend('<p class="cell">' + randomCharacter() + '</p><br/>')
 }
 
-function pickColumn() {
-	return Math.floor(Math.random() * ((Math.floor(window.innerWidth / 10) - 0)) +0);
+function pickColumnNumber() {
+	// Returns a random column number
+	return Math.floor(Math.random() * ((Math.floor(window.innerWidth / 10) - 0))) + 0;
+}
+
+function startMatrix() {
+	setInterval(function(){addCell(pickColumnNumber());}, 1);
+}
+
+function init() {
+	// Gets column number from window width
+	var columns1 = Math.floor(window.innerWidth / 10);
+	var columns = Math.floor(columns1 - (columns1 / 6))
+
+	// Add enough columns to take up full width of window
+	for (i = 0; i < columns; i++) {
+		$("#container").prepend('<div id="col' + i + '" class="column"></div>');
+	}
+	startMatrix();
 }
 
 $(document).ready(function() {
 	init();
+
 });
